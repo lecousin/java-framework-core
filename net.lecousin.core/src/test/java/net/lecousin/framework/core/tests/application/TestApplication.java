8f15b4a8122495ed7c13f1f62ec8dc6af9908634
Guillaume LE COUSIN <g.lecousin-externe@probtp.com>
2017-12-28 10:38:44 +0100

    add tests in TestApplication

--- net.lecousin.core/src/test/java/net/lecousin/framework/core/tests/application/TestApplication.java
+++ net.lecousin.core/src/test/java/net/lecousin/framework/core/tests/application/TestApplication.java
@@ -1,8 +1,11 @@
 package net.lecousin.framework.core.tests.application;
 
+import java.util.Locale;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import net.lecousin.framework.application.Application;
 import net.lecousin.framework.application.LCCore;
 import net.lecousin.framework.concurrent.Task;
 import net.lecousin.framework.core.test.LCCoreAbstractTest;
@@ -10,13 +13,36 @@ import net.lecousin.framework.io.IO;
 
 public class TestApplication extends LCCoreAbstractTest {
 
+	@Test
+	public void testBasic() {
+		Application app = LCCore.getApplication();
+		app.getStartTime();
+		app.getCommandLineArguments();
+		app.getLocale();
+		app.setLocale(Locale.FRENCH);
+		app.getLanguageTag();
+		app.setLocale(Locale.US);
+		app.getLanguageTag();
+		app.getLocalizedProperties();
+	}
+	
 	@SuppressWarnings("resource")
 	@Test
 	public void testResource() throws Exception {
-		IO.Readable io = LCCore.getApplication().getLibrariesManager().getResource("xml-test-suite/mine/001.xml", Task.PRIORITY_NORMAL);
+		Application app = LCCore.getApplication();
+		
+		IO.Readable io = app.getLibrariesManager().getResource("xml-test-suite/mine/001.xml", Task.PRIORITY_NORMAL);
 		Assert.assertFalse(null == io);
 		io.close();
-		io = LCCore.getApplication().getLibrariesManager().getResource("xml-test-suite/mine/doesntexist", Task.PRIORITY_NORMAL);
+		
+		io = app.getLibrariesManager().getResource("xml-test-suite/mine/doesntexist", Task.PRIORITY_NORMAL);
+		Assert.assertTrue(null == io);
+		
+		io = app.getResource("xml-test-suite/mine/001.xml", Task.PRIORITY_NORMAL);
+		Assert.assertFalse(null == io);
+		io.close();
+		
+		io = app.getResource("xml-test-suite/mine/doesntexist", Task.PRIORITY_NORMAL);
 		Assert.assertTrue(null == io);
 	}
 	
