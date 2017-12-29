9e3497c1528e2c2a7c2e16407e4e83b383450a2a
Guillaume LE COUSIN <g.lecousin-externe@probtp.com>
2017-12-29 09:08:33 +0100

    add tests on XMLStreamEventsSync

--- net.lecousin.core/src/test/java/net/lecousin/framework/core/tests/xml/TestXMLStreamEventsSync.java
+++ net.lecousin.core/src/test/java/net/lecousin/framework/core/tests/xml/TestXMLStreamEventsSync.java
@@ -1,5 +1,7 @@
 package net.lecousin.framework.core.tests.xml;
 
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,6 +41,49 @@ public abstract class TestXMLStreamEventsSync extends LCCoreAbstractTest {
 		Assert.assertTrue(xml.getAttributeByLocalName("same") != null);
 		Assert.assertTrue(xml.getAttributeValueByLocalName("same").length() == 0);
 		Assert.assertFalse(xml.nextInnerElement(ctx));
+	}
+	
+	@Test
+	public void testSearchElement() throws Exception {
+		XMLStreamEventsSync xml = parse("xml-test-suite/mine/002.xml");
+		xml.start();
+		Assert.assertTrue(xml.searchElement("spanish"));
+		Assert.assertEquals("hola", xml.getAttributeValueByLocalName("value").asString());
+		xml = parse("xml-test-suite/mine/002.xml");
+		xml.start();
+		Assert.assertFalse(xml.searchElement("german"));
+	}
+	
+	@Test
+	public void testGoInto() throws Exception {
+		XMLStreamEventsSync xml = parse("xml-test-suite/mine/002.xml");
+		xml.start();
+		Assert.assertTrue(xml.nextStartElement());
+		Assert.assertEquals("myRoot", xml.event.text.asString());
+		ElementContext root = xml.event.context.getFirst();
+		Assert.assertTrue(xml.goInto(root, "translation", "hello", "spanish"));
+		Assert.assertEquals("hola", xml.getAttributeValueByLocalName("value").asString());
+		xml = parse("xml-test-suite/mine/002.xml");
+		xml.start();
+		Assert.assertTrue(xml.nextStartElement());
+		Assert.assertEquals("myRoot", xml.event.text.asString());
+		root = xml.event.context.getFirst();
+		Assert.assertFalse(xml.goInto(root, "translation", "hello", "german"));
+	}
+	
+	@Test
+	public void testReadInnerElementsText() throws Exception {
+		XMLStreamEventsSync xml = parse("xml-test-suite/mine/002.xml");
+		xml.start();
+		Assert.assertTrue(xml.nextStartElement());
+		Assert.assertEquals("myRoot", xml.event.text.asString());
+		ElementContext root = xml.event.context.getFirst();
+		Assert.assertTrue(xml.goInto(root, "translation", "hello"));
+		Map<String, String> map = xml.readInnerElementsText(xml.event.context.getFirst());
+		Assert.assertEquals("bonjour", map.get("french"));
+		Assert.assertEquals("", map.get("spanish"));
+		Assert.assertEquals("", map.get("english"));
+		Assert.assertEquals(null, map.get("german"));
 	}
 	
 }
