1d2fcd033acb044d50a84562d017943298b256d7
Guillaume Le Cousin <guillaume@lecousin.net>
2020-02-09 12:01:29 +0100

    do not use LCConcurrentRunner for TestAsyncTimeout for build machines that do not have enough cpus

--- net.lecousin.core/src/test/java/net/lecousin/framework/core/tests/concurrent/util/TestAsyncTimeout.java
+++ net.lecousin.core/src/test/java/net/lecousin/framework/core/tests/concurrent/util/TestAsyncTimeout.java
@@ -7,7 +7,10 @@ import net.lecousin.framework.mutable.MutableBoolean;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
+@RunWith(BlockJUnit4ClassRunner.class)
 public class TestAsyncTimeout extends LCCoreAbstractTest {
 
 	@Test
